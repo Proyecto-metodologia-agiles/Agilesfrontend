16c77dfb863ec1e8ce67a710bcc4e9d6c0c0506e
andr11s <alvisandres1@gmail.com>
2020-10-11 19:51:32 -0500

    Model de miembro de comite creado

---
+++ src/app/index/shared/registrar-comite/registrar-comite.component.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { RegistrarComiteComponent } from './registrar-comite.component';
+
+describe('a registrar-comite component', () => {
+	let component: RegistrarComiteComponent;
+
+	// register all needed dependencies
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [
+				RegistrarComiteComponent
+			]
+		});
+	});
+
+	// instantiation through framework injection
+	beforeEach(inject([RegistrarComiteComponent], (RegistrarComiteComponent) => {
+		component = RegistrarComiteComponent;
+	}));
+
+	it('should have an instance', () => {
+		expect(component).toBeDefined();
+	});
+});
---
+++ src/app/index/shared/registrar-comite/registrar-comite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'registrar-comite',
+	templateUrl: './registrar-comite.component.html',
+	styleUrls: ['./registrar-comite.component.css']
+})
+
+export class RegistrarComiteComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() { }
+
+}
