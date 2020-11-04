b2cf77a2fb96a97219a45c615f6d415b194bd550
andr11s <alvisandres1@gmail.com>
2020-11-03 19:49:02 -0500

    Vista evaluar anteproyecto

---
+++ src/app/index/shared/evaluar-ateproyecto/evaluar-ateproyecto.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'evaluar-ateproyecto',
+	templateUrl: './evaluar-ateproyecto.component.html',
+	styleUrls: ['./evaluar-ateproyecto.component.css']
+})
+
+export class EvaluarAteproyectoComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() { }
+}
